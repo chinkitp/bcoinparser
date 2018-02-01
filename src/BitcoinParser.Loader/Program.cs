b02067d47f92c100d3c806124baf66352d68e309
Chinkit Patel <chinkit@savebigonfuel.com.au>
2018-02-01 20:15:00 +1030

    empty push

---
+++ src/BitcoinParser.Loader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitcoinParser.Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
